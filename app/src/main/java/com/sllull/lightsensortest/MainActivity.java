e693a200223859116bf43e6c9ba45f05a7dd1fd9
Adria Mallol <adria.mallol@gmail.com>
2019-06-05 09:50:35 +0200

    class sensors

--- app/src/main/java/com/sllull/lightsensortest/MainActivity.java
+++ app/src/main/java/com/sllull/lightsensortest/MainActivity.java
@@ -10,52 +10,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView textValues;
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
+public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textValues = findViewById(R.id.value_text);
 
-        mSensorManager= (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)!=null){
-            mSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        }else{
-            //Failure
-            Log.d("sllull2","No light sensor available");
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(mSensor!=null){
-            mSensorManager.registerListener(this,
-                    mSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL
-            );
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        mSensorManager.unregisterListener(this);
-        super.onPause();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        float lux = event.values[0];
-        String oldvalues = textValues.getText().toString();
-        textValues.setText(lux + "\n" + oldvalues );
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+
 }
---
+++ app/src/main/java/com/sllull/lightsensortest/Sensors.java
@@ -0,0 +1,55 @@
+package com.sllull.lightsensortest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class Sensors extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor mLight;
+    private TextView textValues;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textValues = findViewById(R.id.value_text);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = event.values[0];
+        String oldvalues = textValues.getText().toString();
+        textValues.setText(lux + "\n" + oldvalues);
+        // Do something with this sensor value.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+}
